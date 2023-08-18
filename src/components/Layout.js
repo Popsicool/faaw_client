dbe5daf51fe84e1d54bcfb23a9441209153e1125
Popsicool <akinolasamson1234@gmail.com>
2023-08-18 09:37:26 +0100

    show to lilian

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -10,7 +10,7 @@ export const Layout = ({ children }) => {
         <Header/>
         <div>{ children }</div>
         <Footer/>
-        {/* <StickyNavigation/> */}
+        <StickyNavigation/>
       </div>
     </>
   )
