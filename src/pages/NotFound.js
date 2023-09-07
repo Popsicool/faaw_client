f454fc10a598355968ece0f408aee98dad32de5f
Popsicool <akinolasamson1234@gmail.com>
2023-09-07 11:37:30 +0100

    error page added

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import error from "../assets/notFound.jpg"
 import "../styles/notfound.css"
 
 export const NotFound = () => {
   return (
     <div className='notFound'>
-        <div>Comrade Relax, I never code this page abegiiii </div>
-        <div><Link to='/'><button className='btS'>Go home</button></Link></div>
+      <p className="nFBold">Oops!</p>
+      <img src={error} alt="error"/>
+        <p className="nFBold">Sorry, The page you are looking for doesn't exist.</p>
+        <p>If you think something went wrong, please report a problem</p>
+        <div className='errBtn'><Link to='/' className='errorLink'><button className='Ebt'>Go home</button></Link> <Link to='/' className='errorLink'><button className='Ebt Ebt2'>Contact us</button></Link></div>
     </div>
   )
 }
